d456a3d3bd826fee1922b73ffe9c8c34db62f862
Ash <ash@drashsmith.com>
2019-09-18 00:53:32 +0100

    Added some models, and a basic serializer

--- models.py
+++ models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Position(models.Model):
+    lat = models.FloatField()
+    lon = models.FloatField()
+    time = models.DateTimeField()
+    speed = models.IntegerField(blank=True, null=True)
+    explicit = models.BooleanField(default=True)
+    source = models.SlugField(max_length=32)
+    class Meta:
+        app_label = 'locman'
+        verbose_name = 'position'
+        verbose_name_plural = 'positions'
+
+class Location(models.Model):
+    lat = models.FloatField()
+    lon = models.FloatField()
+    description = models.CharField(max_length=128, default='')
+    class Meta:
+        app_label = 'locman'
+        verbose_name = 'location'
+        verbose_name_plural = 'locations'
+
+class Event(models.Model):
+    timestart = models.DateTimeField()
+    timeend = models.DateTimeField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    class Meta:
+        app_label = 'locman'
+        verbose_name = 'event'
+        verbose_name_plural = 'events'
+    
--- views.py
+++ views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-def index(request):
-    return HttpResponse("Hello, world.")
+from .models import *
+from .serializers import *
+
+class EventList(APIView):
+    
+    def get(self, request):
+        data = Event.objects.all()
+        serializer = EventSerializer(data, many=True)
+        return Response(serializer.data)
+
+    def post(self):
+        pass
