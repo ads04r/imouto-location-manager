01e0a44676e55c8656ff70b8df15734a5b76a76c
Ash <ash@drashsmith.com>
2019-10-28 10:16:21 +0000

    Added geojson function to event object

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime, pytz
 
 # Create your models here.
 
@@ -38,7 +39,40 @@ class Event(models.Model):
     timeend = models.DateTimeField()
     location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)
     def geojson(self):
-        return []
+        lasttime = datetime.datetime(1970, 1, 1, 0, 0, 0, tzinfo=pytz.UTC)
+        lastlat = 0.0
+        lastlon = 0.0
+        minlat = 360.0
+        minlon = 360.0
+        maxlat = -360.0
+        maxlon = -360.0
+        track = []
+        geo = []
+        for point in Position.objects.filter(time__gte=self.timestart).filter(time__lte=self.timeend):
+            if (point.time - lasttime).total_seconds() > 90:
+                if len(track) > 1:
+                    geo.append(track)
+                lastlat = 0.0
+                lastlon = 0.0
+                track = []
+            data = [point.lon, point.lat]
+            if (lastlat != point.lat) or (lastlon != point.lon):
+                track.append(data)
+                if point.lon < minlon:
+                    minlon = point.lon
+                if point.lon > maxlon:
+                    maxlon = point.lon
+                if point.lat < minlat:
+                    minlat = point.lat
+                if point.lat > maxlat:
+                    maxlat = point.lat
+            lasttime = point.time
+            lastlat = point.lat
+            lastlon = point.lon
+        if len(track) > 1:
+            geo.append(track)
+        ret = {"type":"Feature", "bbox":[minlon, maxlat, maxlon, minlat], "properties":{}, "geometry":{"type":"MultiLineString","coordinates":geo}}
+        return ret
     class Meta:
         app_label = 'locman'
         verbose_name = 'event'
